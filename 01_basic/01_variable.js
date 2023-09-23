1daa6c51b9fa336f96e3cc25ab1697abdfb15db7
Arjun kumar yadav <123084619+Arjun91029@users.noreply.github.com>
2023-09-23 16:35:03 +0000

    variable and datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1224;
+let accountEmail = "arjun@github.com";
+var accoutnPassword = "12345"
+
+accountCity = "Newtown"
+
+//accountId = 2;
+accountEmail = "aky@ak.com";
+accoutnPassword = "121212";
+accountCity = "Bihar"
+
+/*prefer not to use var
+bacsuse of issue in block scope and function scope */
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountCity, accoutnPassword])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (1+1) // we are using node.js, not browser
+
+let nam
