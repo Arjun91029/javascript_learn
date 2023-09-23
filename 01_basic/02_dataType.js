57d42ce4cb9fd04e3aa638b9b5bd154f56736f84
Arjun kumar yadav <123084619+Arjun91029@users.noreply.github.com>
2023-09-23 16:43:06 +0000

    dataType

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -2,4 +2,21 @@
 
 //alert (1+1) // we are using node.js, not browser
 
-let nam
+let name = "hello"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
