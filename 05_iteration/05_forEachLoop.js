8b6a3d755617c69e90f133c0411f908cabc121a7
Arjun kumar yadav <123084619+Arjun91029@users.noreply.github.com>
2023-10-02 19:27:17 +0000

    For each loop

---
+++ 05_iteration/05_forEachLoop.js
@@ -0,0 +1,35 @@
+const coding = ["js", "py", "cpp", "java", "ruby"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
